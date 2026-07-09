975d4f72d26cc021e5a2a8af7d428cb436e8fb04
Senorita09 <ssarvikel971@gmail.com>
2026-07-09 18:20:19 +0530

    Create using operators.cpp

---
+++ operator/using operators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(){
+  int a,b;
+cout<<"Enter a:";
+cin>>a;
+cout<<"Enter b:";
+cin>>b;
+int sum =a+b;
+cout<<"sum ="<< sum;
+int difference =a-b;
+cout<<"difference ="<<difference;
+int division= a/b;
+cout<<"DIVISION="<<division;
+int multipilcation =a*b;
+cout<<"multipilcation="<< multipilcation;
+return 0;
+}
