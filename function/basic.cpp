f1168caed23ce54cab9b5493fde5b2966190b4c4
Senorita09 <ssarvikel971@gmail.com>
2026-07-10 18:26:54 +0530

    Create basic.cpp

---
+++ function/basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int printhello(){
+    cout<<"hello"<<endl;
+    return 3;
+}
+int main(){
+    int val=printhello();
+    cout<<"value="<<val<<endl;
+
+    return 0;
+}
